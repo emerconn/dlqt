a3fee2058e6c22754804d594762d0a8b96fcadda
Emerson Connelly <emerconn7@gmail.com>
2025-09-07 17:56:58 -0500

    create auth header and send request

--- cmd/dlqt/fetch.go
+++ cmd/dlqt/fetch.go
@@ -22,18 +22,38 @@ func fetch(ctx context.Context, cmd *cli.Command) error {
 		APIEndpoint: cmd.String("api-url") + "/fetch",
 	}
 
+	// get JWT
 	token, err := msal.GetToken(ctx, &msalConfig)
 	if err != nil {
 		return fmt.Errorf("failed to get token: %w", err)
 	}
 	log.Printf("token: %s\n", token)
 
+	// create request and auth header
 	req, err := http.NewRequest("GET", apiConfig.APIEndpoint, nil)
 	if err != nil {
 		return fmt.Errorf("failed to create request: %w", err)
 	}
 	req.Header.Set("Authorization", "Bearer "+token)
 	req.Header.Set("Content-Type", "application/json") // Optional, adjust as needed
+
+	// TODO: add request body with service bus message ID
+
+	// execute request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to execute request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to fetch data: %s", resp.Status)
+	}
+
+	log.Println("request sent successfully")
+
+	// TODO: handle response body
 
 	return nil
 }
