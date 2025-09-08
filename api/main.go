527496e963b8d230e762ede6d21b0f677bc86050
Emerson Connelly <emerconn7@gmail.com>
2025-09-07 23:40:17 -0500

    add handler

--- api/main.go
+++ api/main.go
@@ -9,6 +9,7 @@ func main() {
 	log.Println("starting DLQT API")
 
 	http.Handle("/fetch", AuthMiddleware(http.HandlerFunc(fetchHandler)))
+	http.Handle("/retrig", AuthMiddleware(http.HandlerFunc(retriggerHandler)))
 
 	log.Println("server starting on port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
