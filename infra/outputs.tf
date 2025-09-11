b1fe5fce4c7051d9ee128c04d4ba9dc6b83ebfe1
Emerson Connelly <emerconn7@gmail.com>
2025-09-10 19:01:35 -0500

    add more tofu outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,19 @@
-output "dlqt_api_app_reg_id" {
+output "app_reg_client_id_dlqt_api" {
   description = "app ID of the DLQT API app registration"
   value       = azuread_application.dlqt_api.client_id
 }
 
-output "dlqt_cmd_app_reg_id" {
+output "app_reg_object_id_dlqt_api" {
+  description = "object ID of the DLQT API app registration"
+  value       = azuread_application.dlqt_api.object_id
+}
+
+output "app_reg_client_id_dlqt_cmd" {
   description = "app ID of the DLQT CMD app registration"
   value       = azuread_application.dlqt_cmd.client_id
+}
+
+output "app_reg_object_id_dlqt_cmd" {
+  description = "object ID of the DLQT CMD app registration"
+  value       = azuread_application.dlqt_cmd.object_id
 }
