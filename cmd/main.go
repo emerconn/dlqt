424ab1c098bb5514420546d45d94f9547333f843
Emerson Connelly <emerconn7@gmail.com>
2025-09-21 13:34:31 -0500

    prep v0.3.0 release

--- cmd/main.go
+++ cmd/main.go
@@ -32,7 +32,7 @@ func main() {
 
 	cmd := &cli.Command{
 		Name:                   "dlqt",
-		Version:                "v0.2.4",
+		Version:                "v0.3.0",
 		Usage:                  "Developer tool for interacting with Azure Service Bus DLQ",
 		EnableShellCompletion:  true,
 		UseShortOptionHandling: true,
